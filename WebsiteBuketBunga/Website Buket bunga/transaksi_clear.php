fd5e7c52313d6cd5ff370d4d5e0cd8a739352a34
Yosevira <yoseviramaulidinarahma@saintek.unipdu.ac.id>
2024-12-23 11:57:28 +0700

    Delete WebsiteBuketBunga/Website Buket bunga directory

--- WebsiteBuketBunga/Website Buket bunga/transaksi_clear.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['checkout_products']); // Hapus data checkout
-unset($_SESSION['total_price']); // Hapus total harga
-echo json_encode(['success' => true]);
-?>
